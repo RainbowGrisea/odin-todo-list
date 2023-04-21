644d3dd1e0a968cd226430eea1d0a9b599a50938
mark <kovmark95@gmail.com>
2023-04-21 13:31:41 +0200

    Add functionality to buttons

--- src/index.js
+++ src/index.js
@@ -9,18 +9,20 @@ render function - DOM manipulation
 */
 
 const projectList = [];
-let currentProject = { tasks:[],};
+let currentProject = { };
 
-function Project( name, description ) {
+function Project( name, description, dueDate ) {
   this.name = name;
   this.description = description;
   this.tasks = [];
+  this.dueDate = dueDate
   pushProject( this );
 }
 
-function Task( text, priority ) {
+function Task( text, priority, checked ) {
   this.text = text;
   this.priority = priority || 'normal';
+  this.checked = checked || false ;
   pushTask( this );
 }
 
@@ -54,24 +56,145 @@ function switchProject ( projectNumber ) {
   currentProject = projectList[ projectNumber ];
 }
 
-function render( project ) {
-  console.table( projectList );
-  const title = document.createElement( 'div' );
-  title.classList.add( 'projectTitle' );
-  title.innerHTML = project.name;
+// Update display
+function render( ) {
   
+  const projectListDiv = document.querySelector( '.projectList' );
+  const projectTitle = document.querySelector( '.projectTitle' );
+  const projectDescription = document.querySelector( '.projectDescription' );
+  const taskList = document.querySelector( '.projectTasks' );
+  
+  projectListDiv.innerHTML = '';
+  projectTitle.innerHTML = currentProject.name;
+  projectDescription.innerHTML = currentProject.description;
+  taskList.innerHTML = '';
+
+  // Render selected project and tasks
+  for ( let taskNumber = 0; taskNumber < currentProject.tasks.length; taskNumber++ ) {
+    const taskLi = document.createElement( 'li' );
+    const checkbox = document.createElement( 'input' );
+    const taskLabel = document.createElement( 'label' );
+    
+    checkbox.type = 'checkbox';
+    checkbox.id = 'item' + taskNumber;
+    checkbox.checked = currentProject.tasks[ taskNumber ].checked;
+    taskLabel.innerHTML = currentProject.tasks[ taskNumber ].text;
+    taskLabel.htmlFor = 'item' + taskNumber;
+    
+    taskLi.appendChild( checkbox );
+    taskLi.appendChild( taskLabel );
+    taskList.appendChild( taskLi );
+    
+    // checkbox.addEventListener( 'click', changeCheckedStatus() {
+
+    // } );
+    checkbox.addEventListener( 'click', function changeCheckedStatus() {
+      console.log( this );
+      currentProject.tasks[ taskNumber ].checked = this.checked;
+    });
+  }
+
+  // Render project list on nav bar
+  for ( let proj = 0; proj < projectList.length; proj++ ) {
+    let projectButton = document.createElement( 'button' );
+    projectButton.classList.add( 'button' );
+    projectButton.innerHTML = projectList[ proj ].name;
+    projectButton.addEventListener( 'click', () => {
+      currentProject = projectList[ proj ];
+      render();
+    } )
+    projectListDiv.appendChild( projectButton );
+  };
 }
 
+// function changeCheckedStatus(  ){
+//   console.log( this );
+// }
+
+const createProjectButton = document.querySelector( '.newProjectButton' );
+createProjectButton.addEventListener( 'click', () => {
+  showModal();
+});
+
+const closeButtons = document.querySelectorAll( '.closeButton' );
+for ( let i = 0; i < closeButtons.length; i++ ){
+  closeButtons[ i ].addEventListener( 'click', () => {
+    closeModal( closeButtons[ i ] );
+  })
+}
+
+function showModal( ) {
+  const modal = document.querySelector( '#myModal' );
+  modal.style.display = 'flex';
+}
+
+function closeModal( button ) {
+  let parent;
+  function findModal( button ) {
+    parent = button.parentElement;
+    const classes = Array.from( parent.classList ); 
+    if ( classes.includes( 'modal' ) ) {
+      parent.style.display = 'none';
+    } else { findModal( parent ) }
+  }
+  findModal( button );
+  clearModal( parent )  
+}
+
+function clearModal( modal ) {
+  const inputs = document.querySelectorAll( 'input' );
+  for ( let i = 0; i < inputs.length; i++ ){
+    inputs[ i ].value = '';
+  } 
+}
+
+const addNewProjectButton = document.querySelector( '.modalCreateProjectButton');
+addNewProjectButton.addEventListener( 'click', () => {
+  const modalElement = addNewProjectButton.parentElement;
+  const name = modalElement.querySelector( '#projectName' ).value;
+  const description = modalElement.querySelector( '#projectDescription' ).value;
+  addNewProject( name, description );
+})
+
+function addNewProject ( name, description ){
+  const newProj = new Project( name, description );
+  clearModal();
+  render();
+  closeModal( addNewProjectButton );
+}
+
+function showTaskModal( ) {
+  const taskModal = document.querySelector( '#taskModal' );
+  taskModal.style.display = 'flex';
+}
+
+const addTaskButton = document.querySelector( '.addTaskButton');
+addTaskButton.addEventListener( 'click', () => {
+  showTaskModal();
+})
+
+const addNewTaskButton = document.querySelector( '.taskCreateButton');
+addNewTaskButton.addEventListener( 'click', () => {
+  const modalElement = addNewTaskButton.parentElement;
+  const description = modalElement.querySelector( '#taskDescription' ).value;
+  const priority = modalElement.querySelector( '#taskPriority' ).value;
+  addNewTask( description, priority );
+})
+
+function addNewTask  ( description, priority ){
+  const newTask = new Task( description, priority );
+  clearModal();
+  render();
+  closeModal( addNewTaskButton );
+}
 
 // Dummy projects and tasks
-new Project( 'proj1', 'blablabla' );
-new Project( 'proj2', 'blablabla222' )
-new Project( 'proj3', 'blablabla22312312' )
-new Project( 'proj4', 'blablabla543' )
+const defaultProject = new Project( 'Default project', 'This is a default project with many interesting tasks. Feel free to edit, or create a new project.', '1 week' );
+currentProject = defaultProject;
+defaultProject.tasks.push(  new Task( 'first task', 'high' ), new Task( 'second', 'medium' ), new Task( 'notthethrid', 'low' ), new Task( 'last', 'medium' ) );
+const defaultProject2 = new Project( 'Second project', 'Here we go again. You can switch between projects.', 'Now' );
+currentProject = defaultProject2;
+defaultProject2.tasks.push(  new Task( 'wow', 'low' ), new Task( 'second', 'medium' ), new Task( 'again', 'low' ), new Task( 'fourth', 'medium' ) );
 
-const task1 = new Task( 'leller', 'high' );
-const task2 = new Task( 'second', 'medium' );
-const task3 = new Task( 'notthethrid', 'low' );
-const task4 = new Task( 'lelci', 'medium' );
-
-currentProject = projectList[0];
+currentProject = defaultProject;
+render();
